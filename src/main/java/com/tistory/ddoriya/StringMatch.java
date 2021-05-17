085985ed8fb55c8ebb99a2414c29716aa99abbe4
이상준 <sangjun.lee@playd.com>
2021-05-17 12:06:33 +0900

    refactor:정렬 변경

--- src/main/java/com/tistory/ddoriya/StringMatch.java
+++ src/main/java/com/tistory/ddoriya/StringMatch.java
@@ -34,66 +34,53 @@ public class StringMatch {
 
 	public static void main(String[] args) {
 		String s = "aa", p = "a";
-		System.out.println(isMatch(s,p));
+		System.out.println(isMatch(s, p));
 
 		s = "aa";
 		p = "*";
-		System.out.println(isMatch(s,p));
+		System.out.println(isMatch(s, p));
 
 		s = "aa";
 		p = "*";
-		System.out.println(isMatch(s,p));
+		System.out.println(isMatch(s, p));
 
 		s = "cb";
 		p = "?a";
-		System.out.println(isMatch(s,p));
+		System.out.println(isMatch(s, p));
 
 		s = "acdcb";
 		p = "a*c?b";
-		System.out.println(isMatch(s,p));
+		System.out.println(isMatch(s, p));
 
 		s = "acdcb";
 		p = "a*b";
-		System.out.println(isMatch(s,p));
+		System.out.println(isMatch(s, p));
 	}
 
-	public static boolean isMatch(String s,String p){
+	public static boolean isMatch(String s, String p) {
 
 		boolean[][] dp = new boolean[p.length() + 1][s.length() + 1];
 
-		for(int i = dp.length-1; i >= 0; i--){
-			for(int j = dp[0].length - 1; j >= 0; j--){
-				if(i == dp.length-1 && j == dp[0].length - 1){
+		for (int i = dp.length - 1; i >= 0; i--) {
+			for (int j = dp[0].length - 1; j >= 0; j--) {
+				if (i == dp.length - 1 && j == dp[0].length - 1) {
 					dp[i][j] = true;
-				}
-
-				else if(i == dp.length-1 ){
+				} else if (i == dp.length - 1) {
 					dp[i][j] = false;
-				}
-
-				else if(j == dp[0].length - 1 ){
-					if(p.charAt(i) == '*'){
-						dp[i][j] = dp[i+1][j];
-					}
-					else{
+				} else if (j == dp[0].length - 1) {
+					if (p.charAt(i) == '*') {
+						dp[i][j] = dp[i + 1][j];
+					} else {
 						dp[i][j] = false;
 					}
-				}
-
-				else{
-					if(p.charAt(i) == s.charAt(j)){
-						dp[i][j] = dp[i+1][j+1];
-					}
-
-
-					else if(p.charAt(i) == '?'){
-						dp[i][j] = dp[i+1][j+1];
-					}
-
-					else if(p.charAt(i) == '*'){
-						dp[i][j] = dp[i+1][j] || dp[i][j+1];
-					}
-					else{
+				} else {
+					if (p.charAt(i) == s.charAt(j)) {
+						dp[i][j] = dp[i + 1][j + 1];
+					} else if (p.charAt(i) == '?') {
+						dp[i][j] = dp[i + 1][j + 1];
+					} else if (p.charAt(i) == '*') {
+						dp[i][j] = dp[i + 1][j] || dp[i][j + 1];
+					} else {
 						dp[i][j] = false;
 					}
 
